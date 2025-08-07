2788a09d4e74c31770f80e038de1c029ef2a577e
Juan Jose Sierra <97188972+JuanSierra16@users.noreply.github.com>
2025-08-06 19:01:28 -0500

    Time: 3 ms (65.41%) | Memory: 57.2 MB (38.38%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,10 +1,13 @@
 export function twoSum(nums: number[], target: number): number[] {
+	const seen = new Map<number, number>();
+
 	for (let i = 0; i < nums.length; i++) {
-		for (let j = i + 1; j < nums.length; j++) {
-			if (nums[i] + nums[j] === target) {
-				return [i, j];
-			}
+		const complement = target - nums[i];
+		if (seen.has(complement)) {
+			return [seen.get(complement)!, i];
 		}
+		seen.set(nums[i], i);
 	}
+
 	return [];
 }
