241d97bdc946b538558e068c07dfcb7fed2b952c
Juan Jose Sierra <97188972+JuanSierra16@users.noreply.github.com>
2025-08-06 19:00:48 -0500

    Time: 29 ms (38.94%) | Memory: 57.1 MB (41.35%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,13 +1,10 @@
 export function twoSum(nums: number[], target: number): number[] {
-	let indexes: number[] = []
-		nums.forEach((i, indexI) => {
-			nums.forEach((j, indexJ) => {
-				if(indexI === indexJ) return
-				if(i + j === target){
-					indexes = [indexI, indexJ]
-                    return
-				}
-			})
-		})
-    return indexes;
-};
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = i + 1; j < nums.length; j++) {
+			if (nums[i] + nums[j] === target) {
+				return [i, j];
+			}
+		}
+	}
+	return [];
+}
